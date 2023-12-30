a56ddc21ec3471f76e3067cdae6bad4d4e07b164
Hichama <hicham.afilali@outlook.com>
2023-12-30 21:48:20 +0200

    update files

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// callAPI function that takes the base and exponent numbers as parameters
+var callAPI = (base,exponent)=>{
+    // instantiate a headers object
+    var myHeaders = new Headers();
+    // add content type header to object
+    myHeaders.append("Content-Type", "application/json");
+    // using built in JSON utility package turn object to string and store in a variable
+    var raw = JSON.stringify({"base":base,"exponent":exponent});
+    // create a JSON object with parameters for API call and store in a variable
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+    // make API call with parameters and use promises to get response
+    fetch("https://yctnu3af2l.execute-api.us-east-1.amazonaws.com/dev", requestOptions)
+    .then(response => response.text())
+    .then(result => alert(JSON.parse(result).body))
+    .catch(error => console.log('error', error));
+}
